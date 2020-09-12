a57488f4216b845b290b702d5e2ca937859e272d
fishstack1142 <fishstack1142@gmail.com>
2020-09-12 20:34:32 +0700

    add mutation create user

---
+++ src/models/user.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
--- src/schema/index.js
+++ src/schema/index.js
@@ -1,47 +1,58 @@
 import { gql } from "apollo-server-express";
-
+import User from '../models/user'
 
 //data from db
 const users = [
-    {
-        id: '1',
-        name: 'Somchai1'
-    },
-    {
-        id: '2',
-        name: 'Somchai2'
-    },
-    {
-        id: '3',
-        name: 'Somchai3'
-    }
-]
+  {
+    id: "1",
+    name: "Somchai1",
+  },
+  {
+    id: "2",
+    name: "Somchai2",
+  },
+  {
+    id: "3",
+    name: "Somchai3",
+  },
+];
 
 export const resolvers = {
+  Query: {
+    //return data from database
+    me: (parent, args, context, info) => users[2],
+    user: (parent, args, context, info) => {
+      const id = args.id;
+      const user = User.findById(args.id);
 
-    Query: {
-        //return data from database
-        me: (parent, args, context, info) => users[2],
-        user: (parent, args, context, info) => {
+      return user;
+    },
+    users: (parent, args, context, info) => User.find({}),
+  },
+  Mutation: {
+      signup: async (parent, args, context, info) => {
 
-            const id  = args.id
-            const user = users.find(u => u.id === id )
-
-            return user
-        },
-        users: (parent, args, context, info) => users
-    }
-}
+        return User.create(args)
+      }
+  }
+};
 
 export const typeDefs = gql`
   type Query {
     me: User!
-    user(id: ID!) : User!
+    user(id: ID!): User!
     users: [User]!
   }
 
   type User {
     id: ID!
     name: String!
+    email: String!
+    password: String!
   }
+
+  type Mutation {
+    signup(name: String!, email: String!, password: String!): User
+  }
+
 `;
